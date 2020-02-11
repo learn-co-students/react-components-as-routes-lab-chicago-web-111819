0f5451408d1313a0039f983d4a9bb0834b6c2625
lluisguardiola <lluisguardiola.h@gmail.com>
2020-02-11 13:39:53 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,28 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const renderMovies = (actorMovies) => {
+    return actorMovies.map((movie, index) => <li key={index}>{movie}</li>)
+  }
+
+  const renderActors = () => {
+    return actors.map((actor, index) => {
+      return (
+        <div key={index}>
+          <h1>{actor.name}</h1>
+          <ul>
+            {renderMovies(actor.movies)}
+          </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
